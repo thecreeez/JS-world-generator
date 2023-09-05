7e8527a4831851084586833d6a4b3b782be1fba6
TheCreeeZ <tczdev@gmail.com>
2023-09-05 16:34:38 +0400

    New feature: fillByFunction
    fillByFunction:
    * By using this function you can affect on  world with math logic (just return some value in lambda function)
    fill:
    * Added commentary

--- scripts/init.js
+++ scripts/init.js
@@ -18,6 +18,14 @@ window.onload = () => {
   });
 }
 
+/**
+ * Example: fill(-100, 100, -100, 100, BlockTypes.ONLY_RED)
+ * @param {Integer} x1 
+ * @param {Integer} y1 
+ * @param {Integer} x2 
+ * @param {Integer} y2 
+ * @param {Block} blockType 
+ */
 function fill(x1, y1, x2, y2, blockType) {
 
   let xLength = Math.max(x2 + 1, x1 + 1) - Math.min(x1, x2);
@@ -38,5 +46,41 @@ function fill(x1, y1, x2, y2, blockType) {
         chunk.setBlock(localPos[0], localPos[1], blockType);
       }
     } 
+  }
+}
+
+/**
+ * Example: fillByFunction((x) => Math.sin(x / 5) * 5, -100, 100, BlockTypes.ONLY_RED)
+ * @param {Function} func 
+ * @param {Integer} x1 
+ * @param {Integer} x2 
+ * @param {Block} blockType 
+ */
+function fillByFunction(func, x1, x2, blockType) {
+  let prevHeight = null;
+  for (let x = Math.min(x1, x2); x < Math.max(x1,x2); x++) {
+    let pos = [x, Math.round(func(x))];
+
+    let chunkPos = MathHelper.globalToChunkPos(pos);
+    let localPos = MathHelper.globalToChunkLocalPos(pos);
+
+    let chunk = Application.World.getChunk(chunkPos[0], chunkPos[1]);
+
+    if (chunk) {
+      chunk.setBlock(localPos[0], localPos[1], blockType);
+
+      for (let y = Math.min(pos[1], prevHeight); y < Math.max(pos[1], prevHeight); y++) {
+        let chunkPos = MathHelper.globalToChunkPos([pos[0], y]);
+        let localPos = MathHelper.globalToChunkLocalPos([pos[0], y]);
+
+        let chunk = Application.World.getChunk(chunkPos[0], chunkPos[1]);
+
+        if (chunk) {
+          chunk.setBlock(localPos[0], localPos[1], blockType);
+        }
+      }
+
+      prevHeight = pos[1];
+    }
   }
 }
